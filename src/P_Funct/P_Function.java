682b8927521d76c8f1cf1d988e3311138c82fb20
wushyrussia <denis.sedikov@yandex.ru>
2018-07-17 07:57:21 +0300

    Initial commit

---
+++ src/P_Funct/P_Function.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Packard Bell on 16.07.2018.
+ */
+import java.util.Scanner;
+public class P_Function {
+    static void F_Interface() throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String per_command = sc.next();
+        switch (per_command){
+            case "help":
+        break;
+        case 
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Packard Bell on 16.07.2018.
+ */
+public class main {
+    static void main(String [] args) throws Exception{
+
+    }
+}
